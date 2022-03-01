57860f2300d74d74a1b74157bb486e06a486b2c0
lucas-salves <salves.lucas@outlook.com>
2022-03-01 20:48:19 -0300

    Add DecimalUtil.toDecimal()
    That converts integer currency values to a 2 decimal places BigDecimal.
    This makes it possible to use the subtract() method to subtract the fees from the invoice amount.

---
+++ src/main/java/br/stark/invoicer/util/DecimalUtil.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.stark.invoicer.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ *
+ * @author lucas
+ */
+public class DecimalUtil {
+    public static BigDecimal toDecimal(BigDecimal value){
+        NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+        
+        DecimalFormat formatter = (DecimalFormat) nf;
+        
+        formatter.applyPattern("##,##");
+        
+        String decimal = formatter.format(value);
+        
+        return new BigDecimal(decimal.replaceAll(",", ".")).setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
