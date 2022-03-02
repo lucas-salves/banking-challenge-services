1b1ab72f7bb9189549a9b516729c60c2fbc9310c
lucas-salves <salves.lucas@outlook.com>
2022-03-01 21:07:31 -0300

    Adding the test directory

---
+++ src/test/java/br/stark/invoicer/InvoicerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.stark.invoicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
